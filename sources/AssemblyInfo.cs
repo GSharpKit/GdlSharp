c23657548848927be490ffd1eeefbc29a9a480e0
Mikkel Kruse Johnsen <mikkel@xmedicus.com>
2017-11-17 09:25:28 +0100

    Updated to 3.26.0

---
+++ sources/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Gdl#")]
+[assembly: AssemblyDescription("Gdl-3 bindings for Mono")]
+[assembly: AssemblyCompany("XMedicus ApS")]
+[assembly: AssemblyCopyright("2015, 2016, 2017 XMedicus ApS, Mikkel Kruse Johnsen")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyDelaySign(false)]
