c8d929d5a1740bc40160c99af4317eca38aa88c5
Mikkel Kruse Johnsen <mikkel@xmedicus.com>
2020-12-14 10:47:29 +0100

    Updated to 3.34 and compiling for netstandard2.0

---
+++ sources/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Gdl#")]
+[assembly: AssemblyDescription("Gdl-3 bindings for C#")]
+[assembly: AssemblyCompany("XMedicus Systems ApS")]
+[assembly: AssemblyCopyright("2015, 2016, 2017, 2018, 2019, 2020 XMedicus ApS, Mikkel Kruse Johnsen")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyDelaySign(false)]
